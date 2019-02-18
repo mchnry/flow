8591ace29ed1014060d39e993d0a05658f16daa8
James Pritz <jamiep@ceinetwork.com>
2019-02-18 10:10:40 -0500

    use wildcard in nuget ref to core

---
+++ flow/Analysis/Articulator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkDefine = Mchnry.Flow.Work.Define;
+using LogicDefine = Mchnry.Flow.Logic.Define;
+
+namespace Mchnry.Flow.Analysis
+{
+    public class Articulator
+    {
+        private readonly List<LogicIntent> logicIntents;
+        private readonly WorkDefine.Workflow workflow;
+        private readonly Configuration.Config configuration;
+
+        public Articulator(List<LogicIntent> logicIntents, WorkDefine.Workflow workflow, Configuration.Config configuration)
+        {
+            this.logicIntents = logicIntents;
+            this.workflow = workflow;
+            this.configuration = configuration;
+        }
+
+
+        public string ArticulateActivity(string activityId, bool removeConvention, bool verbose)
+        {
+            string toReturn = string.Empty;
+
+            /*
+                
+            */
+
+            return toReturn;
+        }
+        public string ArticulateTestCase(ActivityTest test, int caseId, bool removeConvention, bool verbose)
+        {
+            string toReturn = string.Empty;
+
+
+            return toReturn;
+        }
+    }
+}
